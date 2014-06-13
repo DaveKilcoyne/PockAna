a0be8fe63644d8c6c61d0d2ae05ce190890d92d3
David Kilcoyne <d.kilcoyne2@nuigalway.ie>
2014-06-13 11:17:39 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-PockAna
-=======
+PockAnat
+========
+
+git clone *URL* //Clone repo
+go into folder cloned & do: 
+git status //Tells which files changes have been made to
+git add . //Add all untracked files to the commit
+git commit -m "Commit"
+git push //Push the changes in the commit to online repo
---
+++ app/app.js
@@ -0,0 +1,51 @@
+var express = require('express'),
+	http = require('http'),
+	io = require('socket.io'),
+	path = require('path'),
+	fs = require('fs'),
+	sizeOf = require('image-size');
+
+var app = express(); 
+
+app.use(express.static(path.join(__dirname, 'public'))); 
+app.get('/', function(req, res){
+	res.sendfile('index.html');
+});
+
+var server = http.createServer(app),
+	io = io.listen(server); 
+
+io.on('connection', function(socket) {
+
+	socket.on('image_name', function(data) {
+
+		var imagePath = "public/body_2/body_2_"+data+"_1_1.jpg";
+		console.log('Recieved image name: '+imagePath);
+		 // var imagePath = "public/images/"+data;
+
+		sizeOf(imagePath, function(err, dimensions) {
+
+			if (err) {
+				console.log("Error getting image size...");
+			} 
+			else {
+			  	fs.readFile(imagePath, function(err, buf) {
+
+					if (err) {
+						console.log("Error reading image file...");
+					} 
+					else {
+						console.log('image sending');
+						socket.emit('image', { buffer: buf, width: dimensions.width, height: dimensions.height });
+					}
+				});
+		  	}
+		});
+	});
+});
+
+server.listen(3000, function(){
+	console.log('listening on *:3000');
+});
+
+
